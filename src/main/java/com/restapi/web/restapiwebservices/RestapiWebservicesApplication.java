e7e89f2cc7d8a130e899bb54f698e997a8d4f5cb
RIZWAAN <rizwaan.gkp@hotmail.com>
2020-02-15 03:09:03 +0530

    Project initial commit

---
+++ src/main/java/com/restapi/web/restapiwebservices/RestapiWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.web.restapiwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/web/restapiwebservices/RestapiWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.web.restapiwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
